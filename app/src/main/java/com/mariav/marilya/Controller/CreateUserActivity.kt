f152d55240818b915cf81919e5ed8803b6d334c3
Shed <13299703+sheidin@users.noreply.github.com>
2019-05-05 21:48:21 +0300

    Setup registration via API

--- app/src/main/java/com/mariav/marilya/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/mariav/marilya/Controller/CreateUserActivity.kt
@@ -2,12 +2,23 @@ package com.mariav.marilya.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.mariav.marilya.R
+import com.mariav.marilya.Services.AuthService
+import kotlinx.android.synthetic.main.activity_create_user.*
 
 class CreateUserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+    }
+
+    fun onCreateUserClicked(view: View) {
+        AuthService.registerUser(this,inpRegEmail.text.toString(),inpRegPass.text.toString()){complete ->
+            if(complete){
+
+            }
+        }
     }
 }
